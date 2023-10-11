2475fc280b4212f3d38fde3f6a3fab58e01ecf53
Juri Linkov <juri@linkov.net>
2023-10-11 09:27:28 +0300

    * lisp/progmodes/project.el (project-mode-line-face): New variable.
    (project-mode-line-format): Use it (bug#66317).

--- lisp/progmodes/project.el
+++ lisp/progmodes/project.el
@@ -1996,6 +1996,9 @@ is part of the default mode line beginning with Emacs 30."
     (define-key map [mode-line down-mouse-1] project-menu-entry)
     map))
 
+(defvar project-mode-line-face nil
+  "Face name to use for the project name on the mode line.")
+
 (defvar project-mode-line-format '(:eval (project-mode-line-format)))
 (put 'project-mode-line-format 'risky-local-variable t)
 
@@ -2006,6 +2009,7 @@ is part of the default mode line beginning with Emacs 30."
      " "
      (propertize
       (project-name project)
+      'face project-mode-line-face
       'mouse-face 'mode-line-highlight
       'help-echo "mouse-1: Project menu"
       'local-map project-mode-line-map))))
