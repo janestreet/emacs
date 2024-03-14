c6800dceaf14e36be264f33f03d1c57e20ecc02c
Juri Linkov <juri@linkov.net>
2024-03-14 19:29:16 +0200

    Context menu for project (bug#69566)
    * lisp/menu-bar.el (menu-bar-project-item): New variable from
    'project-menu-entry'.
    (menu-bar-tools-menu): Use 'menu-bar-project-item'.
    
    * lisp/mouse.el (context-menu-functions): Add
    'context-menu-project' to choice.
    (context-menu-project): New function.
    
    * lisp/progmodes/project.el (project-menu-entry): Remove variable.
    (project-mode-line-map): Use 'menu-bar-project-item' instead of
    'project-menu-entry'.

--- lisp/menu-bar.el
+++ lisp/menu-bar.el
@@ -1808,6 +1808,9 @@ mail status in mode line"))
     (bindings--define-key menu [project-open-file] '(menu-item "Open File..." project-find-file :help "Open an existing file that belongs to current project"))
     menu))
 
+(defvar menu-bar-project-item
+  `(menu-item "Project" ,menu-bar-project-menu))
+
 (defun menu-bar-read-mail ()
   "Read mail using `read-mail-command'."
   (interactive)
@@ -1895,7 +1898,7 @@ mail status in mode line"))
                   :help "Start language server suitable for this buffer's major-mode"))
 
     (bindings--define-key menu [project]
-      `(menu-item "Project" ,menu-bar-project-menu))
+      menu-bar-project-item)
 
     (bindings--define-key menu [ede]
       '(menu-item "Project Support (EDE)"
