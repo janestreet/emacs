e86ae25586d2f7d1b72d9cb5fec2cd5e632a7812
Sean Whitton <spwhitton@spwhitton.name>
2025-06-06 09:53:32 +0100

    VC: Deprecate log-incoming and log-outgoing backend functions
    * lisp/vc/vc-git.el (vc-git-incoming-revision):
    Inline vc-git--fetch-incoming.
    (vc-git--fetch-incoming, vc-git-log-incoming)
    (vc-git-log-outgoing): Delete.
    * lisp/vc/vc-hg.el (vc-hg-mergebase):
    * lisp/vc/vc.el (vc-default-log-incoming)
    (vc-default-log-outgoing): New functions.
    * lisp/vc/vc.el:
    * etc/NEWS: Document the deprecation.

--- lisp/vc/vc-bzr.el
+++ lisp/vc/vc-bzr.el
@@ -803,10 +803,12 @@ If LIMIT is non-nil, show no more than this many entries."
       (indent-region (match-end 0) (point-max) 2)
       (buffer-substring (match-end 0) (point-max)))))
 
+;; FIXME: Implement `vc-bzr-mergebase' and then delete this.
 (defun vc-bzr-log-incoming (buffer remote-location)
   (apply #'vc-bzr-command "missing" buffer 'async nil
 	 (list "--theirs-only" (unless (string= remote-location "") remote-location))))
 
+;; FIXME: Implement `vc-bzr-mergebase' and then delete this.
 (defun vc-bzr-log-outgoing (buffer remote-location)
   (apply #'vc-bzr-command "missing" buffer 'async nil
 	 (list "--mine-only" (unless (string= remote-location "") remote-location))))
