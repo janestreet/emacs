156b4ff2a6ca5070d20af5a8073849ca67ec38df
Sean Whitton <spwhitton@spwhitton.name>
2025-07-25 19:34:04 +0100

    VC: New support for other working trees
    * lisp/vc/vc-git.el (vc-git--read-start-point): New function,
    factored out of vc-git-create-tag.
    (vc-git-create-tag): Use it.
    (vc-git--worktrees, vc-git-known-other-working-trees)
    (vc-git-add-working-tree, vc-git-delete-working-tree)
    (vc-git-move-working-tree):
    * lisp/vc/vc-hg.el (vc-hg-known-other-working-trees)
    (vc-hg-add-working-tree, vc-hg--shared-p)
    (vc-hg-delete-working-tree, vc-hg-move-working-tree): New
    functions.
    * lisp/vc/vc.el: Define API for known-other-working-tree,
    add-working-tree, delete-working-tree and move-working-tree
    backend functions.
    (vc-dir-status-files): New function.
    (project-current-directory-override): Declare.
    (dired-rename-subdir): Autoload.
    (vc-add-working-tree, vc-switch-working-tree)
    (vc-delete-working-tree, vc-move-working-tree): New commands.
    * lisp/vc/vc-hooks.el (vc-prefix-map): Bind them under C-x v.
    
    * doc/emacs/vc1-xtra.texi (Other Working Trees): New node.
    * etc/NEWS: Announce the new commands.
    
    * test/lisp/vc/vc-tests/vc-tests.el
    (vc-test--other-working-trees): New function.
    (vc-test-git07-other-working-trees)
    (vc-test-hg07-other-working-trees): New tests.

--- lisp/vc/vc-hooks.el
+++ lisp/vc/vc-hooks.el
@@ -949,7 +949,11 @@ In the latter case, VC mode is deactivated for this buffer."
   "D"   #'vc-root-diff
   "~"   #'vc-revision-other-window
   "x"   #'vc-delete-file
-  "!"   #'vc-edit-next-command)
+  "!"   #'vc-edit-next-command
+  "w c" #'vc-add-working-tree
+  "w w" #'vc-switch-working-tree
+  "w x" #'vc-delete-working-tree
+  "w R" #'vc-move-working-tree)
 (fset 'vc-prefix-map vc-prefix-map)
 (define-key ctl-x-map "v" 'vc-prefix-map)
 
