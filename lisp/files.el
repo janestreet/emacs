d11357b3a2b307a2d22171b6fb2d567e1c06618c
Spencer Baugh <sbaugh@janestreet.com>
2025-09-23 15:49:06 -0400

    call load-path-filter-function from openp
    This ensures that e.g. locate-file is filtered by this.

--- lisp/files.el
+++ lisp/files.el
@@ -1361,13 +1361,9 @@ NOERROR is equal to `reload'), or otherwise emit a warning."
     ;; file, so we're done.
     (when (and res (eq lh load-history))
       ;; If `require' did nothing, we need to make sure that was warranted.
-      (let* ((file (or filename (symbol-name feature)))
-             (suffixes (get-load-suffixes))
-             (path
-              (if load-path-filter-function
-                  (funcall load-path-filter-function load-path file suffixes)
-                load-path))
-             (fn (locate-file file path suffixes nil)) ;; load-prefer-newer
+      (let* ((fn (locate-file (or filename (symbol-name feature))
+                              load-path (get-load-suffixes) nil
+                              )) ;; load-prefer-newer
              ;; We used to look for `fn' in `load-history' with `assoc'
              ;; which works in most cases, but in some cases (e.g. when
              ;; `load-prefer-newer' is set) `locate-file' can return a
