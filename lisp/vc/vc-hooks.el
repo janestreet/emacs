8a2de9b98490f5be301d99547eb07f28270740cd
Sean Whitton <spwhitton@spwhitton.name>
2025-12-28 12:10:02 +0000

    Automatically detect the VC outgoing base (bug#80006)
    * lisp/vc/vc-git.el (vc-git--current-branch): Rename to ...
    (vc-git-working-branch): ... this.  All uses changed.
    (vc-git-trunk-or-topic-p, vc-git-topic-outgoing-base):
    * lisp/vc/vc-hg.el (vc-hg--working-branch, vc-hg-working-branch)
    (vc-hg-trunk-or-topic-p, vc-hg-topic-outgoing-base):
    * lisp/vc/vc-hooks.el (vc--safe-branch-regexps-p):
    * lisp/vc/vc.el (vc-default-working-branch)
    (vc-default-trunk-or-topic-p, vc--match-branch-name-regexps)
    (vc--outgoing-base, vc--outgoing-base-mergebase)
    (vc--maybe-read-outgoing-base): New functions.
    (vc-diff-outgoing-base): Call vc--outgoing-base-mergebase.
    (vc-root-diff-outgoing-base, vc-diff-outgoing-base): Use
    vc--maybe-read-outgoing-base in interactive specification.
    (working-branch, trunk-or-topic-p, topic-outgoing-base): New
    specifications for backend functions.
    (vc-trunk-branch-regexps, vc-topic-branch-regexps): New
    variables.
    * .dir-locals.el: Commented entries for the new variables.
    * test/lisp/vc/vc-tests/vc-test-misc.el
    (vc-test-match-branch-name-regexps): New test.
    * doc/emacs/vc1-xtra.texi (Outstanding Changes): Document the
    new functionality.
    
    (cherry picked from commit 6e4bceb8ce46488f96f4b162635e785e3a0b7ac1)

--- lisp/vc/vc-hooks.el
+++ lisp/vc/vc-hooks.el
@@ -1159,6 +1159,14 @@ they had none before."
 (defun vc-default-extra-menu (_backend)
   nil)
 
+(defun vc--safe-branch-regexps-p (val)
+  "Return non-nil if VAL is a safe local value for \\+`vc-*-branch-regexps'."
+  (or (eq val t)
+      (and (listp val)
+           (cl-every (lambda (elt)
+                       (or (symbolp elt) (stringp elt)))
+                     val))))
+
 (provide 'vc-hooks)
 
 ;;; vc-hooks.el ends here
