28729072f212da8b796a09264975968ee873df83
Spencer Baugh <sbaugh@janestreet.com>
2026-01-07 09:45:37 -0500

    minibuffer-visible-completions up-down

--- lisp/emacs-lisp/crm.el
+++ lisp/emacs-lisp/crm.el
@@ -238,11 +238,7 @@ with empty strings removed."
   (let* ((map (if require-match
                   crm-local-must-match-map
                 crm-local-completion-map))
-         (map (if minibuffer-visible-completions
-                  (make-composed-keymap
-                   (list minibuffer-visible-completions-map
-                         map))
-                map))
+         (map (minibuffer-visible-completions--maybe-compose-map map))
          input)
     (minibuffer-with-setup-hook
         (lambda ()
