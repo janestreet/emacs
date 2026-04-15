9fe403db1d53762ae0df6a2441b057003590f74b
Sean Whitton <spwhitton@spwhitton.name>
2026-04-15 10:04:58 -0400

    vc-rename-file: New OK-IF-ALREADY-EXISTS parameter
    * lisp/vc/vc.el (vc-delete-file): New NOCONFIRM parameter.
    (vc-rename-file): New OK-IF-ALREADY-EXISTS parameter.
    * lisp/dired-aux.el (dired-rename-file): Pass it.
    * test/lisp/vc/vc-tests/vc-tests.el (vc-test--rename-file): Test
    it.  Also test moving files into an existing directory.
    
    (cherry picked from commit 5347b221da3c2c47df85b9519e367366f2bf1047)

--- lisp/dired-aux.el
+++ lisp/dired-aux.el
@@ -2352,7 +2352,7 @@ unless OK-IF-ALREADY-EXISTS is non-nil."
                  (ignore-errors (vc-responsible-backend file))
                (vc-backend file))
              (ignore-errors (vc-responsible-backend newname)))
-        (vc-rename-file file newname)
+        (vc-rename-file file newname ok-if-already-exists)
       ;; error is caught in -create-files
       (rename-file file newname ok-if-already-exists))
     ;; Silently rename the visited file of any buffer visiting this file.
