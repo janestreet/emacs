d1ce3c6e64f12295dcd647c0322fe50d58b15e10
Sean Whitton <spwhitton@spwhitton.name>
2026-06-22 15:14:18 +0100

    vc-dir--count-outgoing: Bind default-directory to correct value
    * lisp/vc/vc-dir.el (vc-dir--count-outgoing): Bind
    default-directory to the correct value in the idle callback, so
    that we know we count the outgoing revisions for the correct
    repo.
    
    (cherry picked from commit 4797eef57a93dc670dd98cb0d47c5eb8c87c5a3a)

--- lisp/vc/vc-dir.el
+++ lisp/vc/vc-dir.el
@@ -1444,16 +1444,19 @@ uses OVERLAY."
    (run-with-idle-timer
     0.2 nil
     (lambda ()
-      (let ((display-buffer-overriding-action
-             '(display-buffer-no-window (allow-no-window . t)))
-	    (enable-local-variables
-             (if (memq enable-local-variables '(:safe :all nil))
-		 enable-local-variables
-               ;; Ignore other values that query.
-               :safe))
-            (unknown (propertize "<<unknown>>" 'face 'vc-dir-header-value))
-            (buf (generate-new-buffer " *temp*" t))
-            proc)
+      (let* ((default-directory
+	      (buffer-local-value 'default-directory
+				  (overlay-buffer overlay)))
+	     (display-buffer-overriding-action
+              '(display-buffer-no-window (allow-no-window . t)))
+	     (enable-local-variables
+              (if (memq enable-local-variables '(:safe :all nil))
+		  enable-local-variables
+		;; Ignore other values that query.
+		:safe))
+             (unknown (propertize "<<unknown>>" 'face 'vc-dir-header-value))
+             (buf (generate-new-buffer " *temp*" t))
+             proc)
         (with-current-buffer buf
           (condition-case _
               (progn
